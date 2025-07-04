80fe80e6ce80aee255c9f3f606c478b13f40524c
user <alighaffarian9@gmail.com>
2025-07-05 00:48:24 +0330

    add function recv_until_str

--- c/http_server/includes/socket_utilities.h
+++ c/http_server/includes/socket_utilities.h
@@ -1,8 +1,9 @@
 #ifndef SOCK_UTILS
 #define SOCK_UTILS
+#include <stddef.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 
 size_t inet_sock_len(struct sockaddr);
-
+int recv_until_str(int, char *, char *, size_t, int);
 #endif
--- c/http_server/src/socket_utilities.c
+++ c/http_server/src/socket_utilities.c
@@ -1,4 +1,8 @@
+#include "utilities.h"
+#include <errno.h>
 #include <netinet/in.h>
+#include <stddef.h>
+#include <string.h>
 #include <sys/socket.h>
 
 #include <socket_utilities.h>
@@ -8,4 +12,54 @@ size_t inet_sock_len(struct sockaddr sock){
 		case(AF_INET6): return sizeof(struct sockaddr_in6);	
 	}
 	return 0;
+}
+
+int recv_until_str(int fd, char *str, char *buffer, size_t size_of_buffer, int flags){
+	int total_read_bytes = 0;
+	int iter_read_bytes = 0;
+	char *str_offset = 0;
+
+	long str_size = strlen(str);
+	char *end_of_buffer = buffer + size_of_buffer;
+
+	int last_iter_read_bytes = 0;
+	do {
+		buffer += iter_read_bytes;
+
+		if(buffer >= end_of_buffer)
+			goto bad_msg;	
+
+		last_iter_read_bytes = iter_read_bytes;
+		iter_read_bytes = recv(fd, buffer, size_of_buffer - total_read_bytes, flags);
+		total_read_bytes += iter_read_bytes;
+
+		if (iter_read_bytes == 0)
+			goto bad_msg; // pass recv's errno
+		if (iter_read_bytes == -1)
+			return -1;
+		if(iter_read_bytes < str_size){
+			buffer -= iter_read_bytes; //to undo the addition in the beginning of loop
+			continue;
+		}
+
+		//case of the str being sliced in half when recving
+		char char_backup = 0;
+		long end_of_slice = str_size <= iter_read_bytes ? str_size : iter_read_bytes;
+		swap(buffer + end_of_slice, &char_backup);
+		if (( str_offset = strstr(buffer - str_size + 1, str))){
+			swap(buffer + str_size, &char_backup);
+			break;
+		}
+		swap(buffer + end_of_slice, &char_backup);
+
+		
+
+	}while ( ! (str_offset = strstr(buffer, str)));
+
+	str_offset[str_size] = 0;
+	return total_read_bytes;
+
+bad_msg:
+	errno = EBADMSG;
+	return -1;
 }
