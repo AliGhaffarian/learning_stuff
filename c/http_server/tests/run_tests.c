1c134db21f223fbdb6cc7ea31670d2b30338275a
user <alighaffarian9@gmail.com>
2025-07-06 14:28:59 +0330

    use test_source_socket_utilities()

--- c/http_server/tests/run_tests.c
+++ c/http_server/tests/run_tests.c
@@ -1,7 +1,9 @@
 #include "test_source_http.h"
-#include <test_source_utilities.h>
+#include "test_source_utilities.h"
+#include "test_source_socket_utilities.h"
 
 int main(){
 	test_source_utilities();
 	test_source_http();
+	test_source_socket_utilities();
 }
