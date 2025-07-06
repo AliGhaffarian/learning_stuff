ea8082a52e6f3de2b5a1d393483de754a91fd0ec
user <alighaffarian9@gmail.com>
2025-07-06 14:24:22 +0330

    add tear_down_func field to test_context add macro to get test_context from states

--- c/http_server/tests/includes/generic_test_context.h
+++ c/http_server/tests/includes/generic_test_context.h
@@ -5,10 +5,11 @@
 typedef struct test_context_struct{
 	bool success;
 	void(*display_context)(struct test_context_struct *);
+	void (*tear_down_func)(struct test_context_struct*); //to free allocated stuff in test_case, test_result
 	void *test_case;
 	void *test_result;
 }test_context;
-#define TEST_CONTEXT_PTR_FROM_STATES(states) (((test_context *)*states)->test_case)
+#define TEST_CONTEXT_PTR_FROM_STATES(states) ((test_context *)*states)
 #define TEST_CASE_PTR_FROM_STATES(states) (((test_context *)*states)->test_case)
 
 int generic_test_setup(void **);
