4a82a6ad2e5725c424ee0d50ecfa7fa77993eeeb
user <alighaffarian9@gmail.com>
2025-07-07 14:11:57 +0330

    add tests for test_parse_method()

--- c/http_server/tests/src/test_source_http.c
+++ c/http_server/tests/src/test_source_http.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
 //include cmocka
 #include <setjmp.h>
 #include <stdarg.h>
@@ -73,6 +74,34 @@ test_case_http_header_str2enum test_case_list_http_header_str2enum[] = {
 	},
 };
 
+test_case_parse_method test_case_list_parse_method[] = {
+	{
+		.buffer = "GET ",
+		.expected = {
+			.parsed_method = HTTP_GET,
+			.returnval = 4,
+			.resulted_errno = 0
+		}
+	},
+	{
+		.buffer = "HEAD ",
+		.expected = {
+			.parsed_method = HTTP_HEAD,
+			.returnval = 5,
+			.resulted_errno = 0
+		}
+	},
+	{
+		.buffer = "\nGET",
+		.expected = {
+			.parsed_method = HTTP_UNSPEC,
+			.returnval = -1,
+			.resulted_errno = EINVAL
+		}
+	},
+
+};
+
 static test_context test_cases[] = {
 	{.display_context = display_context_parse_next_http_header, .test_case = (void *)&test_case_list_parse_next_http_header[0], .success = false, .tear_down_func = 0},
 	{.display_context = display_context_parse_next_http_header, .test_case = (void *)&test_case_list_parse_next_http_header[1], .success = false, .tear_down_func = 0},
@@ -85,6 +114,10 @@ static test_context test_cases[] = {
 	{.display_context = display_context_http_header_str2enum, .test_case = (void *)&test_case_list_http_header_str2enum[2], .success = false, .tear_down_func = 0},
 	{.display_context = display_context_http_header_str2enum, .test_case = (void *)&test_case_list_http_header_str2enum[3], .success = false, .tear_down_func = 0},
 	{.display_context = display_context_http_header_str2enum, .test_case = (void *)&test_case_list_http_header_str2enum[4], .success = false, .tear_down_func = 0},
+
+	{.display_context = display_context_parse_method, .test_case = (void *)&test_case_list_parse_method[0], .success = false, .tear_down_func = 0},
+	{.display_context = display_context_parse_method, .test_case = (void *)&test_case_list_parse_method[1], .success = false, .tear_down_func = 0},
+	{.display_context = display_context_parse_method, .test_case = (void *)&test_case_list_parse_method[2], .success = false, .tear_down_func = 0},
 };
 
 const struct CMUnitTest source_http_testers[] = {
@@ -99,6 +132,11 @@ const struct CMUnitTest source_http_testers[] = {
 	cmocka_unit_test_prestate_setup_teardown(test_http_header_str2enum, NULL, generic_test_teardown, &test_cases[7]),
 	cmocka_unit_test_prestate_setup_teardown(test_http_header_str2enum, NULL, generic_test_teardown, &test_cases[8]),
 	cmocka_unit_test_prestate_setup_teardown(test_http_header_str2enum, NULL, generic_test_teardown, &test_cases[9]),
+
+
+	cmocka_unit_test_prestate_setup_teardown(test_parse_method, NULL, generic_test_teardown, &test_cases[10]),
+	cmocka_unit_test_prestate_setup_teardown(test_parse_method, NULL, generic_test_teardown, &test_cases[11]),
+	cmocka_unit_test_prestate_setup_teardown(test_parse_method, NULL, generic_test_teardown, &test_cases[12]),
 };
 
 void test_parse_next_http_header(void **states){
@@ -142,6 +180,53 @@ void test_http_header_str2enum(void **states){
 	assert_int_equal(current_test_case->expected.returnval, current_returnval);
 
 	context->success = true;
+}
+
+void test_parse_method(void ** states){
+	test_case_parse_method *current_test_case = (test_case_parse_method *)TEST_CASE_PTR_FROM_STATES(states);
+	http_request tmp_request;
+	make_http_request(&tmp_request);
+
+	test_context *context = TEST_CONTEXT_PTR_FROM_STATES(states);
+	context->test_result = malloc(sizeof(result_parse_method));
+	result_parse_method *ptr_to_context_test_result = (result_parse_method *)context->test_result;
+
+	char *copy_of_buffer = strdup(current_test_case->buffer);
+	long current_returnval = parse_method(copy_of_buffer, &tmp_request) - copy_of_buffer;
+	free(copy_of_buffer);
+	ptr_to_context_test_result->returnval = current_returnval;
+	ptr_to_context_test_result->parsed_method = tmp_request.method;
+	ptr_to_context_test_result->resulted_errno = errno;
+
+	assert_int_equal(current_test_case->expected.resulted_errno, ptr_to_context_test_result->resulted_errno);
+	if(current_test_case->expected.resulted_errno == 0){
+		assert_int_equal(current_test_case->expected.parsed_method, ptr_to_context_test_result->parsed_method);
+		assert_int_equal(current_test_case->expected.returnval, ptr_to_context_test_result->returnval);
+	}
+	context->success = true;
+}
+
+void display_context_parse_method(test_context *context){
+	result_parse_method *expected = &((test_case_parse_method *)context->test_case)->expected;
+	result_parse_method *test_result = (result_parse_method *)context->test_result;
+
+	print_message("expected results:\n");
+	if(expected->resulted_errno){
+		print_message("errno: %d\n", expected->resulted_errno);
+	}
+	else{
+		print_message("parsed method: %s\n", http_method2str[expected->parsed_method]);
+		print_message("returnval: %ld\n", expected->returnval);
+	}
+
+	print_message("got:\n");
+	if(expected->resulted_errno){
+		print_message("errno: %d\n", test_result->resulted_errno);
+	}
+	else{
+		print_message("parsed method: %s\n", http_method2str[test_result->parsed_method]);
+		print_message("returnval: %ld\n", test_result->returnval);
+	}
 }
 
 void display_context_http_header_str2enum(test_context *context){
